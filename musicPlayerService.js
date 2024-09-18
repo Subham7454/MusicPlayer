959440b0a636d50f2ef608a9ea2fcebe3018bc11
Subham <shubhamrawat1843@gmail.com>
2024-09-18 12:56:57 +0530

    added musicService

---
+++ musicPlayerService.js
@@ -0,0 +1,38 @@
+import TrackPlayer, { RepeatMode } from "react-native-track-player";
+import { palyListData } from "./src/constant.ts"
+
+export default async function setupPlayer() {
+    let isSetup = false;
+    try {
+        await TrackPlayer.getCurrentTrack()
+        isSetup = true
+    } catch (error) {
+        await TrackPlayer.setupPlayer()
+        isSetup = true
+    }
+    finally {
+        return isSetup;
+    }
+
+}
+
+export async function addTrack() {
+    await TrackPlayer.add(palyListData)
+    await TrackPlayer.setRepeatMode(RepeatMode.Queue)
+
+}
+
+export async function playbackService() {
+    TrackPlayer.addEventListener(Event.RemotePause, () => {
+        TrackPlayer.pause()
+    })
+    TrackPlayer.addEventListener(Event.RemotePlay, () => {
+        TrackPlayer.play()
+    })
+    TrackPlayer.addEventListener(Event.RemoteNext, () => {
+        TrackPlayer.skipToNext()
+    })
+    TrackPlayer.addEventListener(Event.RemotePrevios, () => {
+        TrackPlayer.skipToPrevious()
+    })
+}
